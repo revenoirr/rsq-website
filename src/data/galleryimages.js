cddb9d2be4b24f891830ff148cdc5d806d1087eb
revenoirr <federico.gym36@gmail.com>
2025-07-25 00:57:49 +0300

    fix5

--- src/data/galleryimages.js
+++ src/data/galleryimages.js
@@ -2,7 +2,7 @@
 export const galleryImages = [
   {
     id: 1,
-    url: 'public/images/3kapitan.png',
+    url: '/images/3kapitan.png',
     title: '3 капитана в ряд',
     description: 'Рэкс Лост и Тандер',
     category: 'battles',
@@ -11,7 +11,7 @@ export const galleryImages = [
   },
   {
     id: 2,
-    url: 'public/images/admiralvdoh.jpg', 
+    url: '/images/admiralvdoh.jpg', 
     title: 'Адмирал Вульф Юларен!',
     description: 'Самый красивый и неповторимый! <3',
     category: 'ships',
@@ -20,7 +20,7 @@ export const galleryImages = [
   },
    {
     id: 3,
-    url: 'public/images/noir numeral.jpg', 
+    url: '/images/noir-numeral.jpg', 
     title: 'Орех и Джедай',
     description: 'На самом деле это Ноир и Нумерал.....',
     category: 'ships',
@@ -29,7 +29,7 @@ export const galleryImages = [
   },
   {
     id: 4,
-    url: 'public/images/11.jpg', 
+    url: '/images/11.jpg', 
     title: '11 Учебный Батальон!',
     description: 'Всегда на страже кадетов!',
     category: 'ships',
@@ -38,7 +38,7 @@ export const galleryImages = [
   },
    {
     id: 5,
-    url: 'public/images/21.jpg', 
+    url: '/images/21.jpg', 
     title: '21 Ударно Штурмовой Батальон',
     description: 'ЗА БАКАРУ!!!!',
     category: 'ships',
@@ -47,7 +47,7 @@ export const galleryImages = [
   },
   {
     id: 6,
-    url: 'public/images/104.jpg', 
+    url: '/images/104.jpg', 
     title: '104 Разведывательный Батальон',
     description: 'Lost forever <3',
     category: 'ships',
@@ -56,7 +56,7 @@ export const galleryImages = [
   },
   {
     id: 7,
-    url: 'public/images/64.jpg', 
+    url: '/images/64.jpg', 
     title: '64 Десантный Батальон',
     description: '64 в сердцах',
     category: 'ships',
@@ -65,8 +65,8 @@ export const galleryImages = [
   },
   {
     id: 8,
-    url: 'public/images/127.jpg', 
-    title: '127 Авиционный Батальон',
+    url: '/images/127.jpg', 
+    title: '127 Авиационный Батальон',
     description: 'Наши асы!',
     category: 'ships',
     categoryLabel: 'Батальоны',
@@ -74,7 +74,7 @@ export const galleryImages = [
   },
   {
     id: 9,
-    url: 'public/images/flot.jpg', 
+    url: '/images/flot.jpg', 
     title: 'Флот',
     description: 'Ньевест где наши поставки!',
     category: 'ships',
@@ -82,99 +82,6 @@ export const galleryImages = [
     date: '15.02.2025'
   },
 ];
-
-
-//   {
-//     id: 3,
-//     url: 'https://images.unsplash.com/photo-1462332420958-a05d1e002413?w=800',
-//     title: 'Клоны 501-го батальона',
-//     description: 'Мотострелковый батальон готовится к высадке на враждебной территории.',
-//     category: 'personnel',
-//     categoryLabel: 'Личный состав',
-//     date: '08.04.2025'
-//   },
-//   {
-//     id: 4,
-//     url: 'https://images.unsplash.com/photo-1506905925346-21bda4d32df4?w=800',
-//     title: 'Планета Шор',
-//     description: 'Пейзаж планеты Шор во время второй военной кампании корпуса.',
-//     category: 'planets',
-//     categoryLabel: 'Планеты',
-//     date: '12.05.2025'
-//   },
-//   {
-//     id: 5,
-//     url: 'https://images.unsplash.com/photo-1472214103451-9374bd1c798e?w=800',
-//     title: 'Совет джедаев',
-//     description: 'Командование ордена Джедаев планирует следующую операцию.',
-//     category: 'personnel',
-//     categoryLabel: 'Личный состав',
-//     date: '28.03.2025'
-//   },
-//   {
-//     id: 6,
-//     url: 'https://images.unsplash.com/photo-1502134249126-9f3755a50d78?w=800',
-//     title: 'Космическая битва',
-//     description: 'Флот Республики сражается с сепаратистскими дроидами в открытом космосе.',
-//     category: 'battles',
-//     categoryLabel: 'Битвы',
-//     date: '05.06.2025'
-//   },
-//   {
-//     id: 7,
-//     url: 'https://images.unsplash.com/photo-1446776877081-d282a0f896e2?w=800',
-//     title: 'AT-TE Walker',
-//     description: 'Тяжёлый шагоход AT-TE 212-го механизированного батальона в боевом походе.',
-//     category: 'equipment',
-//     categoryLabel: 'Техника',
-//     date: '18.04.2025'
-//   },
-//   {
-//     id: 8,
-//     url: 'https://images.unsplash.com/photo-1451187580459-43490279c0fa?w=800',
-//     title: 'Парейн II',
-//     description: 'Малонаселённая планета Парейн II, место проведения третьей кампании.',
-//     category: 'planets',
-//     categoryLabel: 'Планеты',
-//     date: '14.07.2025'
-//   },
-//   {
-//     id: 9,
-//     url: 'https://images.unsplash.com/photo-1517077304055-6e89abbf09b0?w=800',
-//     title: 'Венатор-класс',
-//     description: 'Звёздный разрушитель класса "Венатор" - флагман адмирала Йуларена.',
-//     category: 'ships',
-//     categoryLabel: 'Корабли',
-//     date: '02.03.2025'
-//   },
-//   {
-//     id: 10,
-//     url: 'https://images.unsplash.com/photo-1614728263952-84ea256f9679?w=800',
-//     title: 'Штурм Кейтума',
-//     description: 'Неудачная четвёртая кампания. Штурм укреплений на планете Кейтум.',
-//     category: 'battles',
-//     categoryLabel: 'Битвы',
-//     date: '20.08.2025'
-//   },
-//   {
-//     id: 11,
-//     url: 'https://images.unsplash.com/photo-1446776433605-bb6e8bb1a2ce?w=800',
-//     title: 'Клон-коммандеры',
-//     description: 'Высшее командование клонов корпуса на военном совете.',
-//     category: 'personnel',
-//     categoryLabel: 'Личный состав',
-//     date: '11.06.2025'
-//   },
-//   {
-//     id: 12,
-//     url: 'https://images.unsplash.com/photo-1542621334-a254cf47733d?w=800',
-//     title: 'LAAT Gunship',
-//     description: 'Десантные корабли 127-й авиационной эскадрильи в полёте.',
-//     category: 'equipment',
-//     categoryLabel: 'Техника',
-//     date: '30.05.2025'
-//   }
-// ];
 
 export const galleryStats = {
   total: galleryImages.length,
